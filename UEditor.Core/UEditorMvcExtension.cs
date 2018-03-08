475b888bd22db3aac5c7837d88a9ffb9c957afa0
baiyunchen <baiyunchen@vip.qq.com>
2018-03-08 09:58:46 +0800

    支持配置BasePath

--- UEditor.Core/UEditorMvcExtension.cs
+++ UEditor.Core/UEditorMvcExtension.cs
@@ -5,13 +5,22 @@ namespace UEditor.Core
 {
     public static class UEditorMvcExtension
     {
-        public static void AddUEditorService(this IServiceCollection services, 
-            string configFileRelativePath = "ueditor.json", 
-            bool isCache = true)
+        /// <summary>
+        /// 添加UEditor后端服务
+        /// </summary>
+        /// <param name="services">IServiceCollection</param>
+        /// <param name="configFileRelativePath">配置文件相对路径</param>
+        /// <param name="isCacheConfig">是否缓存配置文件</param>
+        /// <param name="basePath">配置文件、文件存储路径等各种路径的根目录，默认为Web项目的根目录</param>
+        public static void AddUEditorService(this IServiceCollection services,
+            string configFileRelativePath = "ueditor.json",
+            bool isCacheConfig = true,
+            string basePath = "")
         {
 
             Config.ConfigFile = configFileRelativePath;
-            Config.NoCache = isCache;
+            Config.NoCache = isCacheConfig;
+            Config.WebRootPath = basePath;
 
             services.TryAddSingleton<UEditorService>();
         }
